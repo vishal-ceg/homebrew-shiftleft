e00ffb21156f886f41caddd6dc587de0f6de519a
vishal-ceg <vishalceg@gmail.com>
2020-10-29 10:28:17 +0530

    Update filecli.rb

--- Formula/filecli.rb
+++ Formula/filecli.rb
@@ -1,7 +1,9 @@
-require"formula"classFilecli < Formula
+require "formula" classFilecli < Formula
   homepage "https://github.com/vishal-ceg/CLI-demo"
   url "https://github.com/vishal-ceg/CLI-demo/releases/download/v1.1.2/filecli.zip"
   definstall
       inreplace "brew/filecli", "##PREFIX##", "#{prefix}"
       prefix.install "filecli.jar"
-      bin.install "brew/filecli"endend
+      bin.install "brew/filecli"
+end
+end
