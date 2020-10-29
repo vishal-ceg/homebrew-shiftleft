897049d290e0326955e76b26638dc3bfa3edc47d
vishal-ceg <vishalceg@gmail.com>
2020-10-29 13:33:27 +0530

    Update filecli.rb

--- Formula/filecli.rb
+++ Formula/filecli.rb
@@ -2,9 +2,10 @@ class Filecli < Formula
   desc "A Command Line Interface for file"
   homepage "https://github.com/vishal-ceg/CLI-demo"
   url "https://github.com/vishal-ceg/CLI-demo/releases/download/v1.1.4/filecli.zip"
+  depends_on "openjdk"
 
   def install
-       libexec.install "filecli-console.jar"
-       bin.install_symlink libexec/"filecli.sh"  
+      libexec.install Dir['*']
+      bin.write_jar_script libexec/'filecli-console.jar', 'filecli' 
     end
 end
