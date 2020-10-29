587304a15e44d133af8847c5e51e7a1f4352e95f
vishal-ceg <vishalceg@gmail.com>
2020-10-29 11:50:10 +0530

    Update filecli.rb

--- Formula/filecli.rb
+++ Formula/filecli.rb
@@ -1,7 +1,6 @@
 class Filecli < Formula
   desc "A Command Line Interface for file"
   homepage "https://github.com/vishal-ceg/CLI-demo"
-  8eab6eac07886be7dca5604925c2aa959fb40399660e819250ab9daf9a4e4faa
   url "https://github.com/vishal-ceg/CLI-demo/releases/download/v1.1.2/filecli.zip"
 
   def install
