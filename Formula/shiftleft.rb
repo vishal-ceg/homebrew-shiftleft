a6550ad1055be92f86b7f9ccb7f4477bbc0ad77c
vishal-ceg <vishalceg@gmail.com>
2020-12-18 12:27:41 +0530

    Update shiftleft.rb

--- Formula/shiftleft.rb
+++ Formula/shiftleft.rb
@@ -2,6 +2,7 @@ class Shiftleft < Formula
   desc "A Command Line Interface for file"
   homepage "https://github.com/vishal-ceg/shiftleft-cli"
   url "https://github.com/vishal-ceg/shiftleft-cli/releases/download/v1.1.4/shiftleft.zip"
+  version "v1.0.1"
   bottle :unneeded
   def install
       libexec.install Dir['*']
